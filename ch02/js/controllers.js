0fd1fa13589b83faf633a146d262c84193d60572
DeathscytheH <alejandro.deathscythe@gmail.com>
2015-03-24 17:52:47 -0600

    Ch02 - Ejemplo 05 Alert component
    Ultima variante de la directiva.
    Esto se utiliza cuando queremos ejecutar algo dentro del contexto del
    scope padre. Para esto utilizamos el prefijo ’&’.
    En el ejemplo pasamos la función ‘closeAlert’ a la directiva, definida
    por el controlador para cerrar la alerta.

--- ch02/js/controllers.js
+++ ch02/js/controllers.js
@@ -2,9 +2,11 @@
 parking.controller("parkingCtrl", function ($scope) {
     //ng-bind example
     $scope.appTitle = "[Packt] Parking";
+    //Binding the showAlert variable to the scope
+    $scope.showAlert = true;
     //Binding the alert directive to the scope
     $scope.alertTopic = "Something went wrong!";
-    //Errata in the book pag 39
+    //Errata in the book pag 40
     //controllers.js $scope.alertMessage => $scope.descriptionTopic
     $scope.descriptionTopic = "You must inform the plate and the color of the car!";
     //Binding the car's array to the scope
@@ -17,5 +19,8 @@ parking.controller("parkingCtrl", function ($scope) {
         $scope.cars.push(car);
         delete $scope.car;
     };
-    
+    //Binding the closeAlert function to the scope
+    $scope.closeAlert = function(){
+        $scope.showAlert = false;
+    };
 });
